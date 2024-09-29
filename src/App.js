c3553b4d0c09697d874b6b234623761899f54dd9
Luis Abrantes <luis.hsa@gmail.com>
2024-09-29 03:31:16 -0300

    Refactor App component to conditionally render content based on age and isGreen props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
 
 function App() {
+  const age = 17;
+  const isGreen = true;
+
   return (
   <div className="App"> 
-    <Job salary={1000} position="Frontend Developer" company="Amazon"/>
-    <Job salary={1080} position="Junior SDE" company="Google"/>
-    <Job salary={1900} position="Senior SDE" company="Apple"/>
+    {age>=18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+    {<h1 style={{color: isGreen ? 'green' : 'red'}}> If it is green it is ok. If it is red it is not ok.</h1>}
+
+    {isGreen && <button>ENTER</button>}
   </div>
   );
 }
 
-const Job = (props) => {
-  return (
-    <div> 
-      <h1>{props.company}</h1>
-      <h2>{props.position}</h2>
-      <h3>{props.salary}</h3>
-    </div>
-  )
-};
+
 
 
 export default App;
