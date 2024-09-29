2901af50821039146819c86719203bf4991e5557
Luis Abrantes <luis.hsa@gmail.com>
2024-09-29 02:38:29 -0300

    Primeira função de test - User.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App"> Hello World!</div>
+  const name = <h1>Luis</h1>;
+  const age = <h2>17</h2>;
+  const email = <h2>luis.hsa@gmail.com</h2>;
+
+
+  return (<div className="App"> 
+    <User />
+  </div>
   );
 }
+
+const User = () => {
+  return (
+    <div>
+      <h1>Luis</h1>
+      <h2>17</h2>
+      <h2>luis.hsa@gmail.com</h2>
+    </div>
+  );
+};
 
 export default App;
