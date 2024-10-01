4c4d20f7b5a13ee76d9e617bf16441c2337bb577
Luis Abrantes <luis.hsa@gmail.com>
2024-09-30 23:02:32 -0300

    Refactor App component to render a list of users dynamically

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import {User} from './User';
 
 function App() {
-  const names = ["John", "Jane", "Jim"];
-  // const users = [
-  //   { name: "John", age: 30 },
-  //   { name: "Jane", age: 25 },
-  //   { name: "Jim", age: 40 },
-  // ];
+  const users = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+    { name: "Jim", age: 40 },
+  ];
 
   return (
   <div className="App"> 
-    {names.map((name, key) => {
-      return <h1 key={key}> {name} </h1>;
+    {users.map((user, key) => {
+      return <User name={user.name} age={user.age} /> ;
     })}
   </div>
   );
 }
+
+
 
 export default App;
