b4dffc80dc70154e4354be03df8abf88ff647564
Luis Abrantes <luis.hsa@gmail.com>
2024-09-30 23:16:36 -0300

    Refactor App component to render a list of gas planets (or not) dynamically - Episode 3 exercise

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import {User} from './User';
 
 function App() {
-  const users = [
-    { name: "John", age: 30 },
-    { name: "Jane", age: 25 },
-    { name: "Jim", age: 40 },
+  const planets = [
+    {name: "Mars", isGasPlanet: false},
+    {name: "Earth", isGasPlanet: false},
+    {name: "Jupiter", isGasPlanet: true},
+    {name: "Venus", isGasPlanet: false},
+    {name: "Neptune", isGasPlanet: true},
+    {name: "Uranus", isGasPlanet: true},
   ];
 
-  return (
-  <div className="App"> 
-    {users.map((user, key) => {
-      return <User name={user.name} age={user.age} /> ;
-    })}
-  </div>
-  );
+  return <div className="App"> 
+  {planets.map(
+    (planet, key) => !planet.isGasPlanet && <h1> {planet.name} </h1> 
+    // Remove "!" to display gas planets
+  )}
+  </div>;
 }
 
 
