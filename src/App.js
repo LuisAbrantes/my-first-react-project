db3c17633d6b8bc7610f6acca7b97ca989cec4f6
Luis Abrantes <luis.hsa@gmail.com>
2024-09-30 22:44:01 -0300

    Refactor App component to render a list of names dynamically

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 
 function App() {
-  const age = 17;
-  const isGreen = true;
+  const names = ["John", "Jane", "Jim"];
+  // const users = [
+  //   { name: "John", age: 30 },
+  //   { name: "Jane", age: 25 },
+  //   { name: "Jim", age: 40 },
+  // ];
 
   return (
   <div className="App"> 
-    {age>=18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
-    {<h1 style={{color: isGreen ? 'green' : 'red'}}> If it is green it is ok. If it is red it is not ok.</h1>}
-
-    {isGreen && <button>ENTER</button>}
+    {names.map((name, key) => {
+      return <h1 key={key}> {name} </h1>;
+    })}
   </div>
   );
 }
-
-
-
 
 export default App;
